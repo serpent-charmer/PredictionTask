10239d35c0e006347104c881df3985a211e345d9
serpent-charmer <zagirov.robert@yandex.ru>
2022-03-22 11:17:58 +0500

    Уменьшить main метод

--- src/main/java/ru/liga/AlgorithmBot.java
+++ src/main/java/ru/liga/AlgorithmBot.java
@@ -2,11 +2,12 @@ package ru.liga;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.request.SendPhoto;
 import ru.liga.api.exceptions.MissingArgsException;
-import ru.liga.impl.CommandParser;
 import ru.liga.impl.CommandExecutor;
+import ru.liga.impl.CommandParser;
 import ru.liga.output.GraphResult;
 import ru.liga.output.TextResult;
 
@@ -25,50 +26,51 @@ public class AlgorithmBot {
         bot.execute(action);
     }
 
+    private static void handleUpdates(Update update, TelegramBot bot) {
+        long chatId = update.message().chat().id();
+        if (update.message() == null) {
+            return;
+        }
+        try {
+            String cmd = update.message().text();
+            if (cmd.equals("-help")) {
+                sendHelpMessage(bot, chatId);
+                return;
+            }
+            CommandExecutor executor = CommandParser.parse(cmd);
+            executor.registerResultHandler(TextResult.class,
+                    (TextResult o) -> {
+                        sendMessageFromBot(bot, chatId, o.get());
+                    });
+            executor.registerResultHandler(GraphResult.class,
+                    (GraphResult o) -> {
+                        SendPhoto action = new SendPhoto(chatId, o.get());
+                        bot.execute(action);
+                    });
+            executor.eval();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (MissingArgsException e) {
+            sendMessageFromBot(bot, chatId,
+                    String.format("%s\nTry -help command\n", e.getMessage()));
+        } catch (Exception e) {
+//                    e.printStackTrace();
+            sendMessageFromBot(bot, chatId, e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
         String botToken = System.getenv("BOT_TOKEN");
         TelegramBot bot = new TelegramBot(botToken);
         bot.setUpdatesListener(updates -> {
-
             if (updates.size() < 1) {
                 return UpdatesListener.CONFIRMED_UPDATES_NONE;
             }
-
             updates.forEach(u -> {
-                long chatId = u.message().chat().id();
-                if (u.message() == null) {
-                    return;
-                }
-                try {
-                    String cmd = u.message().text();
-                    if (cmd.equals("-help")) {
-                        sendHelpMessage(bot, chatId);
-                        return;
-                    }
-                    CommandExecutor executor = CommandParser.parse(cmd);
-                    executor.registerResultHandler(TextResult.class,
-                            (TextResult o) -> {
-                                sendMessageFromBot(bot, chatId, o.get());
-                            });
-                    executor.registerResultHandler(GraphResult.class,
-                            (GraphResult o) -> {
-                                SendPhoto action = new SendPhoto(chatId, o.get());
-                                bot.execute(action);
-                            });
-                    executor.eval();
-                } catch (NullPointerException e) {
-                    e.printStackTrace();
-                } catch (MissingArgsException e) {
-                    sendMessageFromBot(bot, chatId,
-                            String.format("%s\nTry -help command\n", e.getMessage()));
-                } catch (Exception e) {
-//                    e.printStackTrace();
-                    sendMessageFromBot(bot, chatId, e.getMessage());
-                }
+                handleUpdates(u, bot);
             });
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
-
     }
 
 }
