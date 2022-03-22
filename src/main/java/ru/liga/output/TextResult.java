e8766a21bcb602420400e1dc593c9c897088012b
serpent-charmer <zagirov.robert@yandex.ru>
2022-03-22 11:20:02 +0500

    NPE или OutOfBounds в TextResult

--- src/main/java/ru/liga/output/TextResult.java
+++ src/main/java/ru/liga/output/TextResult.java
@@ -2,12 +2,13 @@ package ru.liga.output;
 
 import ru.liga.api.AlgorithmResult;
 import ru.liga.impl.RatesResult;
+
 import java.util.List;
 
 public class TextResult extends AlgorithmResult<String> {
 
     public String process(List<RatesResult> results) {
-        return results.get(0).toString();
+        return results.stream().findFirst().orElseThrow(() -> new RuntimeException("No results")).toString();
     }
 
 }
